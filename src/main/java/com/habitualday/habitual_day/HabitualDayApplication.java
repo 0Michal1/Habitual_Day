c321b5b1a77de02d114af9887fe52e664a77c178
Michal <m.loszczyk@gmail.com>
2022-05-19 22:31:50 +0200

    Application

---
+++ src/main/java/com/habitualday/habitual_day/HabitualDayApplication.java
@@ -0,0 +1,13 @@
+package com.habitualday.habitual_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitualDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitualDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitualday/habitual_day/HabitualDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitualday.habitual_day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitualDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
