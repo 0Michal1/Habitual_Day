622734996823cf7ce498b21e97144902a6c910be
Michal <m.loszczyk@gmail.com>
2022-06-10 14:53:12 +0200

    Scheduled streak check

--- src/main/java/com/habitualday/habitual_day/services/scheduled/MyHabitStreakCheck.java
+++ src/main/java/com/habitualday/habitual_day/services/scheduled/MyHabitStreakCheck.java
@@ -7,7 +7,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,10 @@ public class MyHabitStreakCheck {
     @Scheduled(cron = "0 0 0 * * *")
     public void myHabitStreakCheck(){
         List<MyHabit> allUsersMyHabits = myHabitRepository.findAll();
+        for (MyHabit myHabit : allUsersMyHabits) {
+            Map<LocalDate, String> streak = myHabit.getStreak();
+        if(!streak.containsKey(LocalDate.now().minusDays(1))) myHabit.setStreak(new HashMap<>());
+        }
+
     }
 }
